a1467c684a66fc988861b9c1e43ceaed6b5f01a4
Michael <michaems@outlook.com>
2020-05-21 23:13:45 +0300

    Some typos fixed.

--- My_first_test.cpp
+++ My_first_test.cpp
@@ -4,9 +4,8 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-	//todo: add more code later.
 	cout << "Hello Everybody!"<<endl;
-	cout<<"Everything starts from first step..."<<endl;
+	cout<<"Everything starts from the first step..."<<endl;
 	cout<<"Later I will add some more code - Object Oriented code."<<endl;
 	return 0;
 }
