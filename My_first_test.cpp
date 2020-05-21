fffe0869a7f528a342dcdfb16a21224a18f933c7
Michael <michaems@outlook.com>
2020-05-21 17:43:36 +0300

    Merge conflict removed

--- My_first_test.cpp
+++ My_first_test.cpp
@@ -6,9 +6,6 @@ using namespace std;
 int main(int argc, char **argv) {
 	//todo: add more code later.
 	cout << "Hello world!"<<endl;
-<<<<<<< HEAD
-=======
 	cout<<"This is another line to print..."<<endl;
->>>>>>> dev
 	return 0;
 }
