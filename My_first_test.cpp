3c3fb5e96428640bb9b548dcc3bfcff9d818953a
Michael <michaems@outlook.com>
2020-05-21 17:18:15 +0300

    Initial commit.
    	- Right now the code only prints "Hello"

---
+++ My_first_test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	//todo: add more code later.
+	cout << "Hello world!"<<endl;
+	cout<<"This is another line to print..."<<endl;
+	return 0;
+}
